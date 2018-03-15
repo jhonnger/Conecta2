cc6cbe8c0d3cb28b04feedf8af932e54c4586f14
Johnny Rogger Cotos Sullon <jhonny_94_17@hotmail.com>
2018-03-15 00:29:54 -0500

    avance

--- Conecta2-cli/src/app/components/chat/chat.component.ts
+++ Conecta2-cli/src/app/components/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgStyle, NgForOf } from '@angular/common';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,16 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  flagChat = true;
 
-  constructor() { }
+  // tslint:disable-next-line:no-inferrable-types
+  tam_chat: number = 500;
+  // tslint:disable-next-line:no-inferrable-types
+  flagChat: boolean = true;
+
+  contactos: any = [];
+
+  contacto: any = {
+    'name': 'juan'
+  };
+
+  constructor(private chatService: ChatService) { }
 
   ngOnInit() {
+
   }
 
 
   minimizarChat() {
     this.flagChat = !this.flagChat;
-    console.log(this.flagChat);
+    if (this.tam_chat === 500) {
+      this.tam_chat = 200;
+    } else {
+      this.tam_chat = 500;
+    }
+    console.log(this.tam_chat);
   }
 }
---
+++ Conecta2-cli/src/app/services/chat.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+// tslint:disable-next-line:import-blacklist
+import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ChatService {
+  // tslint:disable-next-line:no-inferrable-types
+  UrlChat: string = 'https://chat-34f72.firebaseio.com/usuarios.json';
+
+  constructor(private http: Http) { }
+
+  listarContactos() {
+    return this.http.get( this.UrlChat )
+    .map( res => {
+      return res.json();
+    });
+  }
+
+  nuevoContacto(contacto) {
+    const headers = new Headers({
+      'Content-Type' : 'application/json'
+    });
+    return this.http.post(this.UrlChat, JSON.stringify(contacto), {headers} )
+      .map( res => {
+        return res.json();
+      });
+  }
+
+}
